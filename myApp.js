8c374cedc0a548e2d819b7c7a17190dee0aae75e
Glitch (109irvcqzvde) <none>
2020-02-04 08:48:43 +0000

    ⛹🌶 Checkpoint ./myApp.js:10940771/74

--- myApp.js
+++ myApp.js
@@ -27,7 +27,9 @@ app.use(express.static(__dirname + "/public"));
 
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+})
 
 /** 6) Use the .env file to configure the app */
  
